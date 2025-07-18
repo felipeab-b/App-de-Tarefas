186d4bec7352c51d644d9d3af97215cc0c721f53
felipeab-b <felipeabferreira22@gmail.com>
2025-07-18 01:27:31 -0300

    Funcionalidade de login e autenticação usando json

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from db.datarecord import load_users
 
 app = Flask(__name__)
+app.secret_key = 'segredo'
 
 @app.route('/')
 def home():
     return render_template("home.html")
+
+# === Rota aceita GET para "chamar a página" e POST para enviar o formulario ===
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # === Se o form for enviado (requisição POST), recebe as entradas dos inputs ===
+    if request.method == 'POST':
+        user = request.form['user']
+        password = request.form['password']
+        users = load_users()
+
+        # === Verifica se o usuario existe dentro do json e se a senha corresponde a entrada do input ===
+        # === Caso sim, guarda o user na session, é como uma var global que dura enquanto estiver logado ===
+        if user in users and users[user]['password'] == password:
+            session['user'] = user
+            return redirect(url_for('dashboard'))
+        else:
+            return redirect(url_for('login'))
+
+    # === Resultado da requisição GET
+    return render_template("login.html")
+
+@app.route('/dashboard')
+def dashboard():
+    # === Verifica se o user tem uma session, portanto está logado, e caso não, retorna o login ===
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    return f"sucesso {session['user']}"
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ db/datarecord.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+users_file = 'db/users.json'
+
+# === Vai ler os dados do json e usar o .load para transformar em um dict ===
+def load_users():
+    if not os.path.exists(users_file):
+        return{}
+    with open(users_file, 'r') as f:
+        return json.load(f)
+
+# === Função que vai abrir o dict dos users no mode de write(w) e vai voltar para json ===
+def save_users(users):
+    with open(users_file, 'w') as f:
+        json.dump(users, f, indent=4)
+
+# === Linha para testar ===
+# users = load_users()
+# print(f"Caminho do arquivo: {os.path.abspath(users_file)}")
+# print(users)
